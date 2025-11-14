77d1d4884f5fe6a3c4d24157ff9fa179aadb1902
Lauren Villanueva <u20241220012@usco.edu.com>
2025-11-13 19:05:37 -0500

    Create services

---
+++ SpringSecurityRoles/src/main/java/com/usco/SpringSecurityRoles/services/UserService.java
@@ -0,0 +1,27 @@
+package com.usco.SpringSecurityRoles.services;
+
+import com.usco.SpringSecurityRoles.model.User;
+import com.usco.SpringSecurityRoles.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    
+    private final UserRepository userRepository;
+    
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+    
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+    
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
